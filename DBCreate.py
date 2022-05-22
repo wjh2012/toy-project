116a373cc4d2287038acd3f188392d2756728b65
ggomg <wjh2012@naver.com>
2022-05-23 00:41:40 +0900

    db_edit

---
+++ DBCreate.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+con = sqlite3.connect("C:/Users/wjh20/workdpace/discordBot/bot.db")
+cursor = con.cursor()
+"""
+word
+exp
+userID
+userName
+guild
+date
+"""
+sql = "CREATE TABLE IF NOT EXISTS GGOMG_BOT(\
+        word text PRIMARY KEY,\
+        exp text,\
+        userID text,\
+        userName text,\
+        guild text,\
+        Date text)"
+
+cursor.execute(sql)
+con.close()
--- DBcontroller.py
+++ DBcontroller.py
@@ -2,5 +2,15 @@ import sqlite3
 
 con = sqlite3.connect("C:/Users/wjh20/workdpace/discordBot/bot.db")
 cursor = con.cursor()
+"""
+word
+exp
+userID
+userName
+guild
+date
+"""
+sql = " "
 
-sql = "CREATE TABLE knowledge(word text, exp text)"
+cursor.execute(sql)
+con.close()
