c3494377e9483ecb7d77f2d80e37148770bcb4ab
ggomg <wjh2012@naver.com>
2022-05-24 19:03:18 +0900

    DB_final, auto delete

--- botBot.py
+++ botBot.py
@@ -30,7 +30,7 @@ kal={
 }
 
 def checkGuild(guild):
-    if guild.id == Botserver:
+    if guild.id in (Dongma,Botserver,odikim):
         return True
     else:
         return False
@@ -61,51 +61,74 @@ async def on_message(message):
 
 @bot.event
 async def on_command_error(ctx,error):
-    await ctx.send('무슨 뜻인지 모르겠어요!')
+    await ctx.send('무슨 뜻인지 모르겠어요!',delete_after=8.0)
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send('pong')
+    await ctx.send('pong',delete_after=8.0)
 
 #-----------------------------------------------------------------------------------------
 @bot.listen()
 async def on_message(message):
     if checkGuild(message.guild):
-        if message.author == bot.user:
+        if message.author == bot.user or message.author.bot:
             return
         msg = message.content.split()
-        if msg[0].startswith('!!'):
+        if msg[0].startswith('!'):
+            word = msg[0].removeprefix('!')
+
             if word=='배운말':
-                sql = "SELECT word, exp from GGOMG_BOT"
+                sql = "SELECT word, exp, userName from GGOMG_BOT"
                 cursor.execute(sql)
+                exp = cursor.fetchall()
+                final=""
+                for i,j,k in exp:
+                    final += "{:<20}{:<20}({})\n".format(i,j,k)
+                await message.channel.send(final,delete_after=8.0)
+
+            elif word=='삭제':
+                if len(msg)==1:
+                    await message.channel.send('삭제할 단어를 입력해주세요 (!삭제 단어)')
+                else:
+                    word = ' '.join(msg[1:])
+
+                    sql = f"SELECT exp from GGOMG_BOT where word='{word}'"
+                    cursor.execute(sql)
+                    exp = cursor.fetchall()
+
+                    if exp:
+                        word = ' '.join(msg[1:])
+                        sql = f"DELETE FROM GGOMG_BOT WHERE word='{word}'"
+                        cursor.execute(sql)
+                        con.commit()
+                        await message.channel.send(f'{word} 삭제',delete_after=8.0)
+                    else:
+                        await message.channel.send('배우지 않은 단어입니다..',delete_after=8.0)
+
+
             else:
                 if len(msg)==1:
-                    word = msg[0].removeprefix('!!')
                     try:
                         sql = f"SELECT exp from GGOMG_BOT where word='{word}'"
                         cursor.execute(sql)
                         exp = cursor.fetchall()
                         await message.channel.send(f'{exp[0][0]}')
                     except:
-                        await message.channel.send('모르겠어요')
+                        await message.channel.send('모르겠어요 ^^7',delete_after=8.0)
                 else:
-                    word = msg[0].removeprefix('!!')
                     exp = ' '.join(msg[1:])
                     try:
                         sql = f"INSERT INTO GGOMG_BOT VALUES('{word}','{exp}','{message.author.id}','{message.author.name}','{message.guild.name}','{str(datetime.datetime.now())}')"
                         cursor.execute(sql)
                         con.commit()
-                        await message.channel.send(f'새로 배웠어요!\n{word}는(은) {exp}')
+                        await message.channel.send(f'새로 배웠어요!\n{word}는(은) {exp}',delete_after=8.0)
                     except:
-                        sql = f"UPDATE GGOMG_BOT SET exp='{exp}', userID='{message.author.id}', userName='{message.author.name}', guild='{message.guild.name}', date='{str(datetime.datetime.now())}'"
+                        sql = f"UPDATE GGOMG_BOT SET exp='{exp}', userID='{message.author.id}', userName='{message.author.name}', guild='{message.guild.name}', date='{str(datetime.datetime.now())}' WHERE word='{word}'"
                         cursor.execute(sql)
                         con.commit()
-                        await message.channel.send(f'다시 배웠어요!\n{word}은(는) {exp}')
+                        await message.channel.send(f'다시 배웠어요!\n{word}은(는) {exp}',delete_after=8.0)
         else:
             pass
-
-
-            
 
 
 @bot.listen()
@@ -114,7 +137,7 @@ async def on_message(message):
         if message.author == bot.user:
             return
         if message.content=='ㅎㅇ':
-            await message.channel.send(f'안녕하세요 {message.author.name}님!')
+            await message.channel.send(f'안녕하세요 {message.author.name}님!',delete_after=8.0)
 
 @bot.listen()
 async def on_message(message):
@@ -122,7 +145,7 @@ async def on_message(message):
         if message.author == bot.user:
             return
         if message.content=='슉슈슉':
-            await message.channel.send('슉. 슈슉 시. 시발럼아 슈슉. 슉 슉시. 시벌람아. 슉. 시발. 슈슉 슉. 시. 시발. 슉 럼아 슈슉. 시발. 럼아 슉. 슈슉 슉. 슉 시. 시발럼. 아슉 슈슉 슉. 시벌람아. 슉슉. 슈슉 시. 시발럼아 슈슉. 슉 슉시. 시발럼아. 슉. 시발. 슈슉 슉. 시. 시발. 슉 럼아 슈슉. 시발.')
+            await message.channel.send('슉. 슈슉 시. 시발럼아 슈슉. 슉 슉시. 시벌람아. 슉. 시발. 슈슉 슉. 시. 시발. 슉 럼아 슈슉. 시발. 럼아 슉. 슈슉 슉. 슉 시. 시발럼. 아슉 슈슉 슉. 시벌람아. 슉슉. 슈슉 시. 시발럼아 슈슉. 슉 슉시. 시발럼아. 슉. 시발. 슈슉 슉. 시. 시발. 슉 럼아 슈슉. 시발.',delete_after=5.0)
 
 @bot.listen()
 async def on_message(message):
@@ -130,7 +153,7 @@ async def on_message(message):
         if message.author == bot.user:
             return
         if message.content=='따흐흑':
-            await message.channel.send('악!!!')
+            await message.channel.send('악!!!',delete_after=5.0)
 
 @bot.listen()
 async def on_message(message):
@@ -138,7 +161,7 @@ async def on_message(message):
         if message.author == bot.user:
             return
         if message.content=='새끼..':
-            await message.channel.send('기열!')         
+            await message.channel.send('기열!',delete_after=5.0)         
 
 @bot.listen()
 async def on_message(message):
@@ -148,9 +171,10 @@ async def on_message(message):
         if message.content in ('칼', '칼?'):
             onList = checkOnline(message.guild)
             voList = checkVoice(message.guild)
-            a_sub_b = [x for x in onList if x not in voList]
-            final = [x for x in a_sub_b if x.id in kal.values and x.id!=message.author]
-            menList = [x.mention for x in final]
-            await message.channel.send("".join(map(str, menList)))           
+            if onList and voList:
+                a_sub_b = [x for x in onList if x not in voList]
+                final = [x for x in a_sub_b if x.id in kal.values and x.id!=message.author]
+                menList = [x.mention for x in final]
+                await message.channel.send("".join(map(str, menList)),delete_after=5.0)           
 
 bot.run(Token)
--- reusable.py
+++ reusable.py
@@ -1,4 +1,3 @@
-import datetime
 """
 @bot.event
 async def on_ready():
@@ -12,9 +11,9 @@ async def on_member_update(before, after):
         if before.status.value in ('offline','dnd') and after.desktop_status.value=='online':
             await after.guild.text_channels[0].send(f"안녕하세요. {after.name}님!")
 """
+a= [('꼼', '멍청이'), ('관호', '멍청이'), ('냐', '깡통')]
+q=""
+for i,j in a:
+    q+="{:<10}{:<10}\n".format(i,j)
 
-a= [('꼼장오', '멍청이'), ('관호', '멍청이'), ('아냐', '깡통')]
-
-Q = [1.3, 2.4]
-print(', '.join('{:.2f}'.format(f) for f in Q))
-# 1.30, 2.40
+print(q)
